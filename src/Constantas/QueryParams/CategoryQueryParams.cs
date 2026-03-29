431e98bc770b3270801e612fedd1109ac107e531
ibrahim <galuh.ibrahiim@gmail.com>
2026-03-29 19:08:19 +0700

    refactor query param to global function

--- src/Constantas/QueryParams/CategoryQueryParams.cs
+++ src/Constantas/QueryParams/CategoryQueryParams.cs
@@ -1,19 +1,5 @@
 namespace RestAPI.Constantas.QueryParams;
 
-public class CategoryQueryParams
+public class CategoryQueryParams : BaseQueryParams
 {
-    private const int MaxPageSize = 100;
-    private int _pageSize = 10;
-
-    public int Page { get; set; } = 1;
-
-    public int PageSize
-    {
-        get => _pageSize;
-        set => _pageSize = value > MaxPageSize ? MaxPageSize : value < 1 ? 1 : value;
-    }
-
-    public string? Search { get; set; }
-    public string SortBy { get; set; } = "name";
-    public string SortOrder { get; set; } = "asc";
 }
